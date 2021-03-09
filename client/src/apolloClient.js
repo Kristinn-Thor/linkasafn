58007538c6f0edb495019312a5dad2ed7f4c7498
Kristinn <kthk8@hi.is>
2021-03-09 10:06:34 +0000

    útfæra WebSockets fyrir clientin

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -5,10 +5,9 @@ import {
   ApolloLink
 } from '@apollo/client';
 import { useAuthToken } from './components/AuthToken';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
-});
+import { split } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 // Middleware sem setur aðgangslykilinn ef hann er til staðar sem authorization header með hverri fyrirspurn e.request 
 // sem við sendum á serverinn í gegnum Apollo client.
@@ -23,14 +22,39 @@ const authMiddleware = (token) => new ApolloLink((operation, forward) => {
   return forward(operation);
 });
 
-const cache = new InMemoryCache();
-
 export const useApolloClient = () => {
   // Náum í aðgangslykilinn sem er geymdur í vafraköku
   const [token] = useAuthToken();
+
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+  });
+
+  const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/graphql`,
+    options: {
+      reconnect: true,
+      connectionParams: {
+        authToken: token
+      }
+    }
+  });
+
+  const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query);
+      return (
+        kind === 'OperationDefinition' &&
+        operation === 'subscription'
+      );
+    },
+    wsLink,
+    authMiddleware(token).concat(httpLink)
+  );
+
   return new ApolloClient({
-    link: authMiddleware(token).concat(httpLink),
-    cache: cache
+    link,
+    cache: new InMemoryCache()
   });
 
 };
