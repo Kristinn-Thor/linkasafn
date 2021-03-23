48c1b6140d16edddb70a88e23e9b281928219335
Kristinn <kthk8@hi.is>
2021-03-23 15:47:47 +0000

    klára útfærslu á client signUp validation

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,6 +6,8 @@ import { useAuthToken } from './AuthToken';
 import { LOGIN_MUTATION, SIGNUP_MUTATION } from '../mutations';
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm";
+import isEmail from 'validator/lib/isEmail';
+import isAlpha from 'validator/lib/isAlpha';
 
 const Login = () => {
   const history = useHistory();
@@ -34,6 +36,85 @@ const Login = () => {
     error: false,
     message: ''
   });
+  //######################################################## HANDLERS & HELPERS ########################################################
+  //######################################################## CLIENT-SIDE VALIDATION ####################################################
+  const validatePassword = (password) => {
+    // Lykilorð verður að vera 8-200 stafir. Innihalda a.m.k. einn stóran staf og einn lítinn staf. Má innihalda tákn.
+    const isValid = password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,200}$/);
+    if (isValid) return true;
+    return false;
+  };
+
+  function handleFormChange(e, operation, referance) {
+
+    switch (operation) {
+      case 'name':
+        setFormState({ ...formState, name: e.target.value });
+        if (isAlpha(e.target.value) && e.target.value.length >= 2) {
+          e.target.classList.remove('invalid');
+          setValidationState({ ...validationState, name: true });
+        }
+        else {
+          e.target.classList.add('invalid');
+          setValidationState({ ...validationState, name: false });
+        };
+        break;
+
+      case 'email':
+        setFormState({ ...formState, email: e.target.value });
+        if (isEmail(e.target.value)) {
+          e.target.classList.remove('invalid');
+          setValidationState({ ...validationState, email: true });
+        }
+        else {
+          e.target.classList.add('invalid');
+          setValidationState({ ...validationState, email: false });
+        };
+        break;
+
+      case 'password':
+        setFormState({ ...formState, password: e.target.value });
+        if (validatePassword(e.target.value)) {
+          e.target.classList.remove('invalid');
+          setValidationState({ ...validationState, password: true });
+          if (e.target.value !== referance.current.value) {
+            referance.current.classList.add('invalid');
+            setValidationState({ ...validationState, password: true, passwordConf: false });
+          } else {
+            referance.current.classList.remove('invalid');
+            setValidationState({ ...validationState, password: true, passwordConf: true });
+          }
+        }
+        else {
+          e.target.classList.add('invalid');
+          setValidationState({ ...validationState, password: false });
+          if (e.target.value !== referance.current.value) {
+            referance.current.classList.add('invalid');
+            setValidationState({ ...validationState, password: false, passwordConf: false });
+          } else {
+            referance.current.classList.remove('invalid');
+            setValidationState({ ...validationState, password: false, passwordConf: true });
+          }
+        };
+        break;
+
+      case 'password2':
+        setFormState({ ...formState, passwordConf: e.target.value });
+        if (e.target.value === formState.password) {
+          e.target.classList.remove('invalid');
+          setValidationState({ ...validationState, passwordConf: true });
+        }
+        else {
+          e.target.classList.add('invalid');
+          setValidationState({ ...validationState, passwordConf: false });
+        };
+        console.info("Halló frá password2");
+        break;
+
+      default:
+        return;
+    }
+  };
 
   //######################################################## LOG IN HOOK ########################################################
   const [login, { loading: loginLoading }] = useMutation(LOGIN_MUTATION, {
@@ -76,20 +157,28 @@ const Login = () => {
         <SignUpForm
           formState={formState}
           setFormState={setFormState}
-          validationState={validationState}
-          setValidationState={setValidationState} />
+          onUpdate={handleFormChange}
+        />
       )}
       {formState.login && (
         <LoginForm formState={formState} setFormState={setFormState} />
       )}
+      <div className="validation">
+        <ul>
+          {!validationState.name && (<li>Nafn má aðeins innihalda stafi. Verður að vera a.m.k. tveir stafir</li>)}
+          {!validationState.email && (<li>Netfang verður að vera gilt.</li>)}
+          {!validationState.password && (<li>Lykilorð verður að vera a.m.k. 8 að lengd, innihalda a.m.k. einn stóran og einn lítinn staf. Má innihalda tákn.</li>)}
+          {!validationState.passwordConf && (<li>Vinsamlegast staðfestu rétt lykiliorð</li>)}
+        </ul>
+      </div>
       <div>
         <button
           className={(true) ? 'button' : 'button invalid'}
           onClick={(e) => {
             if (!formState.login) {
               if (validationState.name && validationState.email && validationState.password && validationState.passwordConf) {
-                return console.info("The form is Valid : )", validationState); // login
-              } else return console.info("The form is invalid", validationState);
+                return console.info("The form is Valid : )", validationState); // signup
+              } else return console.info("The form is invalid", validationState); // sýna "validation" reglur
             } else return console.info(`Logging in user ${formState.email}`)//login;
           }}
         >
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,31 +1,15 @@
 import React, { useRef } from "react";
-import isEmail from 'validator/lib/isEmail';
-import isAlpha from 'validator/lib/isAlpha';
 
-const SignUpForm = ({ formState, setFormState, validationState, setValidationState }) => {
-  //######################################################## CLIENT-SIDE VALIDATION #########################################
-  const validatePassword = (password) => {
-    // Lykilorð verður að vera 8-200 stafir. Innihalda a.m.k. einn stóran staf og einn lítinn staf. Má innihalda tákn.
-    const isValid = password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,200}$/);
-    if (isValid) return true;
-    return false;
-  };
-  const password2 = useRef(null);
+const SignUpForm = ({ formState, onUpdate }) => {
+
+  const password2 = useRef();
 
   return (
     <div className="form logIn">
       <input className="input"
         value={formState.name}
         onChange={(e) => {
-          setFormState({ ...formState, name: e.target.value });
-          if (isAlpha(e.target.value) && e.target.value.length >= 2) {
-            e.target.classList.remove('invalid');
-            setValidationState({ ...validationState, name: true });
-          }
-          else {
-            e.target.classList.add('invalid');
-            setValidationState({ ...validationState, name: false });
-          };
+          onUpdate(e, 'name');
         }}
         type="text"
         placeholder="Nafn"
@@ -33,15 +17,7 @@ const SignUpForm = ({ formState, setFormState, validationState, setValidationSta
       <input className="input"
         value={formState.email}
         onChange={(e) => {
-          setFormState({ ...formState, email: e.target.value });
-          if (isEmail(e.target.value)) {
-            e.target.classList.remove('invalid');
-            setValidationState({ ...validationState, email: true });
-          }
-          else {
-            e.target.classList.add('invalid');
-            setValidationState({ ...validationState, email: false });
-          };
+          onUpdate(e, 'email');
         }}
         type="email"
         placeholder="Netfang"
@@ -50,16 +26,7 @@ const SignUpForm = ({ formState, setFormState, validationState, setValidationSta
         className="input"
         value={formState.password}
         onChange={(e) => {
-          setFormState({ ...formState, password: e.target.value });
-          console.info(password2.current.value);
-          if (validatePassword(e.target.value) && (e.target.value === password2.current.value || password2.current.value === '')) {
-            e.target.classList.remove('invalid');
-            setValidationState({ ...validationState, password: true });
-          }
-          else {
-            e.target.classList.add('invalid');
-            setValidationState({ ...validationState, password: false });
-          };
+          onUpdate(e, 'password', password2);
         }}
         type="password"
         placeholder="Lykilorð"
@@ -69,15 +36,7 @@ const SignUpForm = ({ formState, setFormState, validationState, setValidationSta
         className="input"
         value={formState.passwordConf}
         onChange={(e) => {
-          setFormState({ ...formState, passwordConf: e.target.value });
-          if (e.target.value === formState.password) {
-            e.target.classList.remove('invalid');
-            setValidationState({ ...validationState, passwordConf: true });
-          }
-          else {
-            e.target.classList.add('invalid');
-            setValidationState({ ...validationState, passwordConf: false });
-          };
+          onUpdate(e, 'password2');
         }}
         type="password"
         placeholder="Staðfesta lykilorð"
