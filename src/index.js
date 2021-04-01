a687e89202e161a39cd32ed882fd7c64ddb4b58f
Kristinn <kthk8@hi.is>
2021-04-01 11:31:32 +0000

    laga path

--- src/index.js
+++ src/index.js
@@ -45,12 +45,12 @@ server.applyMiddleware({ app, path: '/graphql' });
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build"));
   });
 } else {
   app.use(express.static(path.join(__dirname, "/client/public")));
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/public"));
   });
 }
 
